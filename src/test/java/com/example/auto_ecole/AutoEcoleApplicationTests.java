9a7228d954df0f8e0378be3a17d7577c97ded8a3
unknown <you@example.com>
2025-06-06 02:04:23 +0200

    Initial commit

---
+++ src/test/java/com/example/auto_ecole/AutoEcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_ecole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
